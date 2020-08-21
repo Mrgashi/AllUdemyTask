5800b3818feb29ad160800e23e21db3007d752ad
MirMir <mirdon_@hotmail.com>
2020-08-21 14:24:58 +0200

    Adding ExceturoService to manage 3 threads

--- ProducerConsumer/src/src/com/udemy/threads/Main.java
+++ ProducerConsumer/src/src/com/udemy/threads/Main.java
@@ -1,9 +1,7 @@
 package src.com.udemy.threads;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.*;
 
 import static src.com.udemy.threads.Main.EOF;
 import static src.com.udemy.threads.ThreadColor.*;
@@ -14,29 +12,46 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        List<String> buffer = new ArrayList<String>();
-        ReentrantLock bufferLock = new ReentrantLock();
-        MyProducer producer = new MyProducer(buffer, ANSI_RED, bufferLock);
-        MyConsumer consumer1 = new MyConsumer(buffer, ANSI_GREEN, bufferLock);
-        MyConsumer consumer2 = new MyConsumer(buffer, ANSI_YELLOW, bufferLock);
+        ArrayBlockingQueue<String> buffer = new ArrayBlockingQueue<String>(6);
+        // ExecutorService only allows 3 threads to run. changing this to 5 would make the future run whenever.
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        MyProducer producer = new MyProducer(buffer, ANSI_RED);
+        MyConsumer consumer1 = new MyConsumer(buffer, ANSI_GREEN);
+        MyConsumer consumer2 = new MyConsumer(buffer, ANSI_YELLOW);
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
 
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ANSI_WHITE + "I am beeing printed form the callable class.");
+                return "This is the callable result";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        } catch (ExecutionException e) {
+            System.out.println("Something went wrong");
+        } catch (InterruptedException e) {
+            System.out.println("Thread running the task was interrupted.");
+        }
+
+        executorService.shutdown();
     }
 }
 
 class MyProducer implements Runnable {
 
-    private List<String> buffer;
+    private ArrayBlockingQueue<String> buffer;
     private String color;
-    private ReentrantLock bufferLock;
 
-    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
+    public MyProducer(ArrayBlockingQueue<String> buffer, String color) {
         this.buffer = buffer;
         this.color = color;
-        this.bufferLock = bufferLock;
     }
 
     @Override
@@ -47,10 +62,7 @@ class MyProducer implements Runnable {
         for (String num : nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                bufferLock.lock();
-                buffer.add(num);
-                bufferLock.unlock();
-
+                buffer.put(num);
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
                 System.out.println("Producer was interupted...");
@@ -58,41 +70,45 @@ class MyProducer implements Runnable {
         }
 
         System.out.println(color + "Adding End of Array/File/data and Exiting");
-        bufferLock.lock();
-        buffer.add("EOF");
-        bufferLock.unlock();
+        try {
+            buffer.put("EOF");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
 class MyConsumer implements Runnable {
 
-    private List<String> buffer;
+    private ArrayBlockingQueue<String> buffer;
     private String color;
-    private ReentrantLock bufferLock;
 
 
-    public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
+    public MyConsumer(ArrayBlockingQueue<String> buffer, String color) {
         this.buffer = buffer;
         this.color = color;
-        this.bufferLock = bufferLock;
     }
 
     @Override
     public void run() {
+
         while (true) {
-            bufferLock.lock();
-            if (buffer.isEmpty()) {
-                bufferLock.unlock();
-                continue;
+            synchronized (buffer) {
+                try {
+                    if (buffer.isEmpty()) {
+                        continue;
+                    }
+                    if (buffer.peek().equals(EOF)) {
+                        System.out.println(color + "Exiting");
+                        break;
+                    } else {
+                        System.out.println(color + "Removed." + buffer.take());
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    System.out.println(e.getMessage());
+                }
             }
-            if (buffer.get(0).equals(EOF)) {
-                System.out.println(color + "Exiting");
-                bufferLock.unlock();
-                break;
-            } else {
-                System.out.println(color + "Removed." + buffer.remove(0));
-            }
-            bufferLock.unlock();
         }
     }
 }
